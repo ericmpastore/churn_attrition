3be45e202ac61bf1ab0be796f6d93fb0093b3290
Eric P <epas001@hotmail.com>
2026-07-06 23:20:11 +0200

    Added file source to script

--- attrition.py
+++ attrition.py
@@ -19,8 +19,8 @@ def load_db(in_file,table_name,db_path):
 def main():
     # Declare Constants, EPastore 05/17/2026
     SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-    CSV_FILE = os.path.join(SCRIPT_DIR,'employee_satisfaction_survey.csv')
-    TABLE_NAME = 'employee_satisfaction'
+    CSV_FILE = os.path.join(SCRIPT_DIR,'WA_Fn-UseC_-HR-Employee-Attrition.csv')
+    TABLE_NAME = 'employee_attrition'
     DB_PATH = os.path.join(SCRIPT_DIR,"my_database.duckdb")
 
     # Create table in database, EPastore 05/17/2026
@@ -37,25 +37,24 @@ def main():
         """))
     
     # Business Question
-    # Isolate each employee's most recent response, then count how many employees fall into each satisfaction rating, EPastore 07/03/2026
-    # Primary Business Question: How many employees reported satisfaction scores of "1" on their most recent survey responses?
+    # Determine attrition levels, EPastore 07/06/2026
 
-    print(
-        con.sql(
-            f"""
-            WITH latest AS 
-            (
-                SELECT Email, Satisfaction
-                FROM employee_satisfaction
-                QUALIFY ROW_NUMBER() OVER (PARTITION BY Email ORDER BY Timestamp DESC) = 1
-            )
+#     print(
+#         con.sql(
+#             f"""
+#             WITH latest AS 
+#             (
+#                 SELECT Email, Satisfaction
+#                 FROM employee_satisfaction
+#                 QUALIFY ROW_NUMBER() OVER (PARTITION BY Email ORDER BY Timestamp DESC) = 1
+#             )
 
-            SELECT Satisfaction, COUNT(Email) AS Employees
-            FROM latest
-            GROUP BY Satisfaction;
-"""
-        )
-    )
+#             SELECT Satisfaction, COUNT(Email) AS Employees
+#             FROM latest
+#             GROUP BY Satisfaction;
+# """
+#         )
+#     )
 
 if __name__ == '__main__':
     main()
